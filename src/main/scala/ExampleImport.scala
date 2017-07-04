4d8dcc3072190cd244ea89046d86fc4f42c135f1
Sam Wischnewsky <syw1@williams.edu>
2017-07-04 11:21:31 -0500

    Added code to allow CsvReader to load file but still throwing up error

--- src/main/scala/ExampleImport.scala
+++ src/main/scala/ExampleImport.scala
@@ -1,27 +1,17 @@
-//To begin using file open Scala interpretive mode.  For Scala version 2.12 use :require /User/.../univocity-parsers-2.4.1.jar
-
 import com.univocity.parsers.csv._
 import java.io._
 
 
 object ExampleImport {
 
-  def sqr(x : Int): Int = x * x
-
   def main(args: Array[String]) = {
-    val value = 3
-
-    println(s"The square of " + value + " is " + sqr(value))
-
-    println(s"The square of $value is ${sqr(value)}")
 
     val settings = new CsvParserSettings()
     val parser = new CsvParser( settings )
 
-    println( "Enter File Directory for CSV to be read" )
-    val input = scala.io.StdIn.readLine()
 
-    val parsedCsv = parser.parseAll( new InputStreamReader(java.lang.System.in) )
-    print( parsedCsv )
+
+    val parsedCsv = parser.parseAll( new InputStreamReader( this.getClass().getResourceAsStream("~/Chicago-Data-Compiler/data/sample.csv"), "UTF-8") )
+    println( parsedCsv )
   }
 }
